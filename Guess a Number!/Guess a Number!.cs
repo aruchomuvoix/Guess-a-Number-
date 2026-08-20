88e7dd881c2b78f28b67b5dd6c25f589cffb0a29
aruchomu <artemzakharov659@gmail.com>
2026-08-20 18:27:07 +0500

    Added a thing that checks what you entered.

--- Guess a Number!/Guess a Number!.cs
+++ Guess a Number!/Guess a Number!.cs
@@ -7,100 +7,20 @@ namespace Guess_a_Number_
 {
     class Program
     {
-        static async Task Main()
-        {
-            Thread.Sleep(1000);
-
-            Console.WriteLine("Guess a Number! 1.0.0\n");
-
-
-
-
-            Thread.Sleep(1000);
-
-            string text = "Welcome to the Guess a Number! (Made by Aruchomu).\n";
-
-            foreach (char c in text)
+            static async Task Main()
             {
-                Console.Write(c);
-                await Task.Delay(80);
-            }
+                await Task.Delay(1000);
+
+                Console.WriteLine("Guess a Number! 1.0.0\n");
 
 
 
 
+                await Task.Delay(1000);
 
-            Thread.Sleep(1000);
+                string text = "Welcome to the Guess a Number! (Made by Aruchomu).\n";
 
-            string text1 = "\nThe rules are easy-peasy: AI picks some number and you need to guess it!";
-
-            foreach (char c in text1)
-            {
-                Console.Write(c);
-                await Task.Delay(80);
-            }
-
-
-
-
-
-            Thread.Sleep(1000);
-
-            string text3 = "\nAlso, you have only 1 attempt, because I don't feel like to give you more! ;)";
-
-            foreach (char c in text3)
-            {
-                Console.Write(c);
-                await Task.Delay(80);
-            }
-
-
-
-
-
-
-            Thread.Sleep(1500);
-
-            string text2 = "\n\nPlease, press any key to start.";
-
-            foreach (char c in text2)
-            {
-                Console.Write(c);
-                await Task.Delay(80);
-            }
-
-
-
-
-
-
-            var letter = Console.ReadKey();
-
-            Console.Clear();
-
-
-
-
-
-
-            Console.WriteLine("Please, guess a number from 0 to 100!:");
-
-
-
-
-            int a = Random.Shared.Next(0, 101);
-
-            int user_input = Convert.ToInt32(Console.ReadLine());
-
-
-            if (a == user_input)
-            {
-                Thread.Sleep(4000);
-
-
-                string b = "Correct! You guessed the number!\nIt was " + a + "!";
-
-                foreach (char c in b)
+                foreach (char c in text)
                 {
                     Console.Write(c);
                     await Task.Delay(80);
@@ -108,18 +28,13 @@ namespace Guess_a_Number_
 
 
 
-                Console.WriteLine("\n\nPress any key to exit...");
-
-                Console.ReadKey();
-            }
-            else
-            {
-                Thread.Sleep(4000);
 
 
-                string d = "NOPE! That's not it.\nIt was " + a + "!";
+                await Task.Delay(1000);
 
-                foreach (char c in d)
+                string text1 = "\nThe rules are easy-peasy: AI picks some number and you need to guess it!";
+
+                foreach (char c in text1)
                 {
                     Console.Write(c);
                     await Task.Delay(80);
@@ -127,10 +42,103 @@ namespace Guess_a_Number_
 
 
 
-                Console.WriteLine("\n\nPress any key to exit...");
 
-                Console.ReadKey();
+
+                await Task.Delay(1000);
+
+                string text3 = "\nAlso, you have only 1 attempt, because I don't feel like to give you more! ;)";
+
+                foreach (char c in text3)
+                {
+                    Console.Write(c);
+                    await Task.Delay(80);
+                }
+
+
+
+
+
+
+                await Task.Delay(1500);
+
+                string text2 = "\n\nPlease, press any key to start.";
+
+                foreach (char c in text2)
+                {
+                    Console.Write(c);
+                    await Task.Delay(80);
+                }
+
+
+
+
+
+
+                var letter = Console.ReadKey();
+
+                Console.Clear();
+
+
+
+
+
+
+                Console.WriteLine("Please, guess a number from 0 to 100!:");
+
+
+
+
+                int a = Random.Shared.Next(0, 101);
+
+                int user_input;
+
+            while (!int.TryParse(Console.ReadLine(), out user_input))   
+            {
+                Console.WriteLine("Invalid input. Please, enter your number again:");
+            }
+
+
+                if (a == user_input)
+                {
+                    await Task.Delay(4000);
+
+
+                    string b = "Correct! You guessed the number!\nIt was " + a + "!";
+
+                    foreach (char c in b)
+                    {
+                        Console.Write(c);
+                        await Task.Delay(80);
+                    }
+
+
+
+                    Console.WriteLine("\n\nPress any key to exit...");
+
+                    Console.ReadKey();
+                }
+                else
+                {
+                    await Task.Delay(4000);
+
+
+                    string d = "NOPE! That's not it.\nIt was " + a + "!";
+
+                    foreach (char c in d)
+                    {
+                        Console.Write(c);
+                        await Task.Delay(80);
+                    }
+
+
+
+                    Console.WriteLine("\n\nPress any key to exit...");
+
+                    Console.ReadKey();
+                }
             }
         }
     }
-}
+
+
+
